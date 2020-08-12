3597996c2a651f9297615c4afe537ac90fccc9ec
yassinrais <yassmailrais@gmail.com>
2020-08-12 14:13:59 +0200

    A : sakura-cli -a create-plugin (create plugin by a template) A : create-plugin template

---
+++ cli/templates/create-plugin/plugin.config.php
@@ -0,0 +1,36 @@
+<?php 
+
+use SakuraPanel\Library\Plugins\Plugin;
+
+$plugin = new Plugin();
+
+$plugin->initPluginByJson(dirname(__FILE__)."/plugin.config.json");
+
+// $plugin->addRoute(
+// 	"member", 
+// 	"city" ,
+// 	[
+// 		'url'=>['/#/@','/#/@/','/#/@/:action','/#/@/:action/:params'],
+// 	    'controller'=>"\SakuraPanel\Plugins\\${groupName}\Controllers\City",
+// 	    'action'=>1 , 
+// 	    'params'=>2 , 
+// 	    'access' => ['admins' => ['*'] ]
+// 	]
+// )
+// ->addMenu(
+// 	"Geo",
+// 	"city",
+// 	[
+// 		"title"=>"Cities",
+// 		"icon"=>"fas fa-map",
+// 		"url"=>"member/city",
+// 		"access"=>"admins",
+// 	]
+// )
+// ->addSql([
+// 	'install'=>dirname(__FILE__).'/sql/install.sql',
+// 	'delete'=>dirname(__FILE__).'/sql/delete.sql'
+// ])
+// ;
+
+return $plugin;
