74d1832c6cfde68cfd086426827ab37e14a1197e
yassinrais <yassmailrais@gmail.com>
2020-08-14 20:01:47 +0200

    A : migrations 1.0.1  ( Issue : #5 )

---
+++ app/migrations/1.0.1/auth_security.php
@@ -0,0 +1,115 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Migrations\Mvc\Model\Migration;
+
+/**
+ * Class AuthSecurityMigration_101
+ */
+class AuthSecurityMigration_101 extends Migration
+{
+    /**
+     * Define the table structure
+     *
+     * @return void
+     */
+    public function morph()
+    {
+        $this->morphTable('auth_security', [
+                'columns' => [
+                    new Column(
+                        'id',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'notNull' => true,
+                            'autoIncrement' => true,
+                            'size' => 11,
+                            'first' => true
+                        ]
+                    ),
+                    new Column(
+                        'suspend',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'default' => "0",
+                            'notNull' => true,
+                            'size' => 1,
+                            'after' => 'id'
+                        ]
+                    ),
+                    new Column(
+                        'attemps',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'default' => "0",
+                            'notNull' => true,
+                            'size' => 11,
+                            'after' => 'suspend'
+                        ]
+                    ),
+                    new Column(
+                        'last_attemps',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'default' => "0",
+                            'notNull' => true,
+                            'size' => 11,
+                            'after' => 'attemps'
+                        ]
+                    ),
+                    new Column(
+                        'banned',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'default' => "0",
+                            'notNull' => true,
+                            'size' => 1,
+                            'after' => 'last_attemps'
+                        ]
+                    ),
+                    new Column(
+                        'ip',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 200,
+                            'after' => 'banned'
+                        ]
+                    )
+                ],
+                'indexes' => [
+                    new Index('PRIMARY', ['id'], 'PRIMARY')
+                ],
+                'options' => [
+                    'TABLE_TYPE' => 'BASE TABLE',
+                    'AUTO_INCREMENT' => '3',
+                    'ENGINE' => 'MyISAM',
+                    'TABLE_COLLATION' => 'utf8_bin'
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Run the migrations
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+
+}
