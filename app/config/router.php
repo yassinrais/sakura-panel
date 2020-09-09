626f21d474c4a4c93d670322f8b370a446f98f17
yassinrais <yassmailrais@gmail.com>
2020-09-09 23:51:21 +0200

    Fix : Merge Router namespace parameter #14

--- app/config/router.php
+++ app/config/router.php
@@ -13,16 +13,21 @@ foreach ($configs->route_groups as $prefix => $rgroups) {
     foreach ($rgroups as $name => $page) {
         $urls = is_object($page->url) ? $page->url : (object) [$page->url];
         foreach ($urls as $url) {
+            $route = [
+                'controller' => 
+                    str_replace('[M]', 'Sakura\Controllers\Member', 
+                        $page->controller
+                    ),
+                'action'     => (!empty($page->action)) ? $page->action : 'index',
+                'params'     => (!empty($page->params)) ? $page->params : null,
+            ];
+            foreach(['namespace'] as $key){
+                if(!empty($page->$key))
+                    $route[$key] = $page->$key;
+            }
             $router->add(
                 str_replace("@", $name,  str_replace("#", $prefix, $url)),
-                [
-                    'controller' => 
-                        str_replace('[M]', 'Sakura\Controllers\Member', 
-                            $page->controller
-                        ),
-                    'action'     => (!empty($page->action)) ? $page->action : 'index',
-                    'params'     => (!empty($page->params)) ? $page->params : null,
-                ]
+                $route
             );
         }
     }
