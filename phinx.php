4b9fc605fd4a32f1827878c9b9d28ba986872afc
yassinrais <yassmailrais@gmail.com>
2020-11-02 15:01:31 +0100

    🐱‍👤 - Init migration created

--- phinx.php
+++ phinx.php
@@ -1,18 +1,22 @@
 <?php
 include 'vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable('./');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-return
-[
+
+var_dump(getenv());
+
+
+return [
     'paths' => [
-        'migrations' => '%%PHINX_CONFIG_DIR%%/resources/migrations',
-        'seeds' => '%%PHINX_CONFIG_DIR%%/resources/seeds'
+        'migrations' => '%%PHINX_CONFIG_DIR%%/resources/database/migrations',
+        'seeds' => '%%PHINX_CONFIG_DIR%%/resources/database/seeds'
     ],
     'environments' => [
         'default_migration_table' => 'phinxlog_migrations',
-        'default_environment' => 'development',
+        'default_environment' => 'production',
+        
         'production' => [
             'adapter' => strtolower(getenv('DB_ADAPTER')) ?: 'mysql',
             'host' => getenv('DB_HOST') ?: 'localhost',
@@ -24,20 +28,20 @@ return
         ],
         'development' => [
             'adapter' => strtolower(getenv('DB_ADAPTER')) ?: 'mysql',
-            'host' => getenv('DB_HOST') ?: 'localhost',
-            'name' => getenv('DB_NAME') ?: 'development_db',
-            'user' => getenv('DB_USER') ?: 'root',
-            'pass' => getenv('DB_PASS') ?: '',
-            'port' => getenv('DB_PORT') ?: '3306',
+            'host' => getenv('DB_HOST_DEV') ?: 'localhost',
+            'name' => getenv('DB_NAME_DEV') ?: 'development_db',
+            'user' => getenv('DB_USER_DEV') ?: 'root',
+            'pass' => getenv('DB_PASS_DEV') ?: '',
+            'port' => getenv('DB_PORT_DEV') ?: '3306',
             'charset' => 'utf8',
         ],
         'testing' => [
             'adapter' => strtolower(getenv('DB_ADAPTER')) ?: 'mysql',
-            'host' => getenv('DB_HOST') ?: 'localhost',
-            'name' => getenv('DB_NAME') ?: 'testing_db',
-            'user' => getenv('DB_USER') ?: 'root',
-            'pass' => getenv('DB_PASS') ?: '',
-            'port' => getenv('DB_PORT') ?: '3306',
+            'host' => getenv('DB_HOST_TESTING') ?: 'localhost',
+            'name' => getenv('DB_NAME_TESTING') ?: 'testing_db',
+            'user' => getenv('DB_USER_TESTING') ?: 'root',
+            'pass' => getenv('DB_PASS_TESTING') ?: '',
+            'port' => getenv('DB_PORT_TESTING') ?: '3306',
             'charset' => 'utf8',
         ]
     ],
